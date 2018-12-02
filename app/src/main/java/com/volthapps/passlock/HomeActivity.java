bc15ebef4c8a5b92361b3be1981331182569a9a6
Oscar Arranz <theikkiscout@gmail.com>
2018-12-02 11:22:41 -0800

    Borrado el boton de atrás en Home

--- app/src/main/java/com/volthapps/passlock/HomeActivity.java
+++ app/src/main/java/com/volthapps/passlock/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.volthapps.passlock;
 
+import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +14,8 @@ import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
-    Button btn;
+    Button backButton;
+    Button settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +23,18 @@ public class HomeActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.action_bar);
+        Toolbar toolbar = findViewById(R.id.action_bar);
         setSupportActionBar(toolbar);
 
-        btn = findViewById(R.id.backButton);
-        btn.setVisibility(View.INVISIBLE);
-
+        backButton = findViewById(R.id.backButton);
+        backButton.setVisibility(View.INVISIBLE);
+        settingsButton = findViewById(R.id.settingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(HomeActivity.this, SettingsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 
--- app/src/main/java/com/volthapps/passlock/SettingsActivity.java
+++ app/src/main/java/com/volthapps/passlock/SettingsActivity.java
@@ -1,4 +1,33 @@
 package com.volthapps.passlock;
 
-public class SettingsActivity {
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.action_bar);
+        setSupportActionBar(toolbar);
+
+        btn = findViewById(R.id.settingsButton);
+        btn.setVisibility(View.INVISIBLE);
+    }
+
+
+
 }
