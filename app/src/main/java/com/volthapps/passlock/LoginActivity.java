ae31b91716b5197cd17bfd6f9354bc8a5d2aab32
PedritoGM <pedrogarciasuanzes@gmail.com>
2018-12-02 20:49:32 +0100

    Añadido el boton atras en el login activity para que cierre la app

--- app/src/main/java/com/volthapps/passlock/LoginActivity.java
+++ app/src/main/java/com/volthapps/passlock/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.volthapps.passlock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -13,5 +14,14 @@ public class LoginActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+    }
+
+    //Esta funcion hace que se cierre la app cuando le das atras en el login activity
+    public void onBackPressed(){
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+
     }
 }
