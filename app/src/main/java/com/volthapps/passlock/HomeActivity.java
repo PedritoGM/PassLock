889979671d20d05a6e9a2573a2943f32612dafc9
PedritoGM <pedrogarciasuanzes@gmail.com>
2018-12-03 20:35:16 +0100

    Añadida la splash screen

--- app/src/main/java/com/volthapps/passlock/HomeActivity.java
+++ app/src/main/java/com/volthapps/passlock/HomeActivity.java
@@ -19,6 +19,8 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Este settheme es del splashscreen
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
