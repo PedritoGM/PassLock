50f90a1bc092a08adc3bfe414ba936e369d9cd84
Oscar Arranz <theikkiscout@gmail.com>
2018-12-04 11:45:28 -0800

    Mejor splash + bug arreglado boton atras

--- app/src/main/java/com/volthapps/passlock/RegisterActivity.java
+++ app/src/main/java/com/volthapps/passlock/RegisterActivity.java
@@ -49,9 +49,12 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    public void register() {
-        Intent i = new Intent(RegisterActivity.this, HomeActivity.class);
-        startActivity(i);
+    public void onBackPressed(){
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+
     }
 
 }
